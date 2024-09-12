3b231a75e79eaf86c30f399e5657c052b0a58870
HAMZA SHOUL <HSHOUL@groupebcp.com>
2024-09-12 15:01:52 +0100

    add terraform config files

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  /*cloud {
+    organization = "hamzashoul"
+    workspaces {
+      name = "docker-tuto"
+    }
+  }*/
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+
+
+provider "docker" {
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  #name  = "nginx-created-by-terraform-test"
+  name = var.container_name
+  ports {
+    internal = 80
+    external = 8090
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "container_name" {
+  description = "Value of the name of container"
+  type        = string
+  default     = "ExempleNginxContainer"
+}
